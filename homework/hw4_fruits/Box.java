879ef68bd8ac7a1440dec4f9ad8faa5b290e9f55
Olesya Solovyova <soe611@yandex.ru>
2023-03-07 17:00:58 +0700

    homework4 completed

--- homework/hw4_fruits/Box.java
+++ homework/hw4_fruits/Box.java
@@ -1,33 +1,20 @@
 package homework.hw4_fruits;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Box<T extends Fruit> {
+public class Box<T extends Fruit> implements Iterable<T> {
 
     // Нужно реализовать хранение фруктов в коробке Box
     private final List<T> fruits = new ArrayList<>();
 
     public void add(T fruit) {
         fruits.add(fruit);
-        // throw new UnsupportedOperationException();
     }
-
-    public void remove(T fruit) {
-        fruits.remove(fruit);
-    }
-
-    public int count() {
-        return fruits.size();
-    }
-
-    public T get(Integer index) {
-        return fruits.get(index);
-    }
-
-    double sum = 0;
 
     public double getWeight() {
+        double sum = 0;
         for (T fruit : fruits) {
             sum += fruit.getWeight();
         }
@@ -35,18 +22,26 @@ public class Box<T extends Fruit> {
     }
 
     public void moveTo(Box<? super T> anotherBox) {
-        int len = this.count();
-        for (int i = 0; i < len; i++) {
-            anotherBox.add(this.get(i));
+        for (int i = 0; i < fruits.size(); i++) {
+            anotherBox.add(fruits.get(i));
         }
-        for (int i = 0; i < len; i++) {
-            this.remove(get(i));
-            // }
-            // for (T fruit : fruits) {
-            // anotherBox.add(fruit);
-            // this.remove(fruit);
-            // }
-        }
-
+        fruits.clear();
     }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> iter = new Iterator<T>() {
+            int i = 0;
+
+            public boolean hasNext() {
+                return i < fruits.size();
+            }
+
+            public T next() {
+                return fruits.get(i);
+            }
+        };
+        return iter;
+    }
+
 }
